ee853f520f250a29b268a77d238a1de344398552
felipemartins8058 <felipecosmemartins10@gmail.com>
2020-05-22 16:48:33 -0300

    Styled Components Media Links

--- src/components/Layout/styled.js
+++ src/components/Layout/styled.js
@@ -4,7 +4,7 @@ export const LayoutWrapper = styled.section`
   display:flex;
 `
 export const LayoutMain = styled.main`
-  background: #444444;
+  background: #16202c;
   min-height: 100vh;
   padding: 0 3.75rem 0 20rem;
   width: 100%;
---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,26 @@
+const handle = "felipe.martins"
+
+const links = [
+    {
+        label :"Github",
+        url:`blabla/${handle}`
+    },
+    {
+        label :"Twitter",
+        url:`blabla/${handle}`
+    },
+    {
+        label :"Unsplash",
+        url:`blabla/${handle}`
+    },
+    {
+        label :"Instagram",
+        url:`blabla/${handle}`
+    },
+    {
+        label :"Youtube",
+        url:`blabla/${handle}`
+    }
+]
+
+export default links
