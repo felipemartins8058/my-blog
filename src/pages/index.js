9a7c38edf7d7a1cf386f9ec379ff15319efb85be
felipemartins8058 <felipecosmemartins10@gmail.com>
2020-05-27 18:10:31 -0300

    Post item

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,19 @@ import React from "react"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
-
+import PostItem from "../components/PostItem"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Home</h1>
-    <p>Mezametamae waga uruji tachi yo.</p>
+    <PostItem
+    slug="/slug/"
+    background="blue"
+    category="MIH"
+    date="7/10/1999"
+    timeToRead="Finnaly I made it..."
+    title="Made In Heaven"
+    description="I'm taking my ride with destiny. Willing to play my part. Living with painful memories!"
+    />
   </Layout>
 )
 
