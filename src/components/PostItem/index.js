ea8250f171db7c57c1ee1c4089046ce40bc7e68a
felipemartins8058 <felipecosmemartins10@gmail.com>
2020-05-29 11:04:18 -0300

    Passando os dados do GraphQL para a página

--- src/components/PostItem/index.js
+++ src/components/PostItem/index.js
@@ -15,7 +15,7 @@ const PostItem = ({
         <S.PostItemWrapper>
             <S.PostItemTag background={background}>{category}</S.PostItemTag>
             <S.PostItemInfo>
-                <S.PostItemDate>{date} | {timeToRead}</S.PostItemDate>
+                <S.PostItemDate>{date} | {timeToRead} min de leitura</S.PostItemDate>
                 <S.PostItemTitle>{title}</S.PostItemTitle>
                 <S.PostItemDescription>{description}</S.PostItemDescription>
             </S.PostItemInfo>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,49 @@
 import React from "react"
+import { useStaticQuery, graphql } from  "gatsby"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 import PostItem from "../components/PostItem"
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <PostItem
-    slug="/slug/"
-    background="blue"
-    category="MIH"
-    date="7/10/1999"
-    timeToRead="Finnaly I made it..."
-    title="Made In Heaven"
-    description="I'm taking my ride with destiny. Willing to play my part. Living with painful memories!"
-    />
+const IndexPage = () => {
+  const {allMarkdownRemark} = useStaticQuery(graphql`
+  query PostList {
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            background
+            category
+            date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+            description
+            title
+          }
+          timeToRead
+        }
+      }
+    }
+  }
+  `)  
+
+  const postList = allMarkdownRemark.edges
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      {postList.map(({node: {frontmatter: { background,category,date,description,title},
+      timeToRead
+      }}) => (
+      <PostItem
+        slug="/slug/"
+        background={background}
+        category={category}
+        date={date}
+        timeToRead={timeToRead}
+        title={title}
+        description={description}
+      />
+      ))}
   </Layout>
-)
+  )
+}
 
 export default IndexPage
