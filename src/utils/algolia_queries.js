c2e59ae492070dbf6490de1cf0550130cd81432d
felipemartins8058 <felipecosmemartins10@gmail.com>
2020-06-17 09:04:23 -0300

    Instalando o Algolia

---
+++ src/utils/algolia_queries.js
@@ -0,0 +1,49 @@
+const { array } = require("prop-types")
+
+const postQuery = `
+    {
+        posts: allMarkdownRemark (
+            sort: {fields: frontmatter___date, order: DESC}
+            ) {
+          edges {
+            node {
+              frontmatter {
+                category
+                date_timestamp: date
+                date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+                description
+                title
+              }
+              timeToRead
+              objectID: id
+              fields {
+                slug
+              }
+              excerpt(pruneLength: 50000)
+            }
+          }
+        }
+    }
+`
+
+const flatten = arr =>
+  arr.map(({ node: { frontmatter, ...rest } }) => ({
+    ...frontmatter,
+    date_timestamp: parseInt(
+      (new Date(frontmatter.date_timestamp).getTime() / 1000).toFixed(0)
+    ),
+    ...rest,
+  }))
+
+const queries = [
+  {
+    query: postQuery,
+    transformer: ({ data }) => flatten(data.posts.edges),
+    indexName: "Posts",
+    settings: {
+      attributesToSnippet: ["exerpt:20"],
+    },
+  },
+]
+
+module.exports = queries
