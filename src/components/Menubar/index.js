735a7d1701b819aa7a828a89c884e0303e63b0fd
felipemartins8058 <felipecosmemartins10@gmail.com>
2020-07-02 21:06:56 -0300

    Criando Metodo para Troca de Temas

--- src/components/Menubar/index.js
+++ src/components/Menubar/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Home } from "@styled-icons/boxicons-solid/Home"
 import { SearchAlt2 as Search } from "@styled-icons/boxicons-regular/SearchAlt2"
 import { UpArrowAlt as Arrow } from "@styled-icons/boxicons-regular/UpArrowAlt"
@@ -6,22 +6,51 @@ import { Bulb as Light } from "@styled-icons/boxicons-regular/Bulb"
 import { Grid } from "@styled-icons/boxicons-solid/Grid"
 import * as S from "./styled"
 
-const Menubar = () => (
+const Menubar = () => {
+    const [theme, setTheme] = useState(null)
+
+    const isDarkMode = theme === 'dark'
+
+    useEffect(() => {
+        setTheme(window.__theme)
+        window.__onThemeChange = () => setTheme(window.__theme)
+    },[])
+
+  return (
     <S.MenuBarWrapper>
-        <S.MenuBarGroup>
-            <S.MenuBarLink to="/" title="Voltar para Home">
-                <S.MenuBarItem><Home/></S.MenuBarItem>
-            </S.MenuBarLink>
-            <S.MenuBarLink to="/search/" title="Killer Queen has already touched this button">
-                <S.MenuBarItem><Search/></S.MenuBarItem>
-            </S.MenuBarLink>
-        </S.MenuBarGroup>
-        <S.MenuBarGroup>
-            <S.MenuBarItem title="Mudar o tema"><Light/></S.MenuBarItem>
-            <S.MenuBarItem title="Mudar vizualização"><Grid/></S.MenuBarItem>
-            <S.MenuBarItem title="Ir para o topo"><Arrow/></S.MenuBarItem>
-        </S.MenuBarGroup>
+      <S.MenuBarGroup>
+        <S.MenuBarLink to="/" title="Voltar para Home">
+          <S.MenuBarItem>
+            <Home />
+          </S.MenuBarItem>
+        </S.MenuBarLink>
+        <S.MenuBarLink
+          to="/search/"
+          title="Killer Queen has already touched this button"
+        >
+          <S.MenuBarItem>
+            <Search />
+          </S.MenuBarItem>
+        </S.MenuBarLink>
+      </S.MenuBarGroup>
+      <S.MenuBarGroup>
+        <S.MenuBarItem
+          title="Mudar o tema"
+          onClick={() => {
+            window.__setPreferredTheme(isDarkMode ? 'light' : 'dark')
+          }}
+        >
+          <Light />
+        </S.MenuBarItem>
+        <S.MenuBarItem title="Mudar vizualização">
+          <Grid />
+        </S.MenuBarItem>
+        <S.MenuBarItem title="Ir para o topo">
+          <Arrow />
+        </S.MenuBarItem>
+      </S.MenuBarGroup>
     </S.MenuBarWrapper>
-)
+  )
+}
 
 export default Menubar
